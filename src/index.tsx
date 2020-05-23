749de790819eb630815cea2ceff5e995e786f7de
delyethan <h.delyethan123@gmail.com>
2020-05-23 16:05:56 +0700

    auto height

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
-import { WebView } from 'react-native-webview';
-
+import AutoHeightWebView from 'react-native-autoheight-webview'
 import katexStyle from './katex-style';
 import katexScript from './katex-script';
 
@@ -46,6 +45,7 @@ window.onerror = e => document.write(e);
 window.onload = () => katex.render(${JSON.stringify(expression)}, document.body, ${JSON.stringify(options)});
 ${katexScript}
 </script>
+<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">
 </head>
 <body>
 </body>
@@ -81,12 +81,15 @@ export interface KatexProps extends ContentOptions {
 }
 
 export default function Katex({ style, onLoad, onError, ...options }: KatexProps) {
-  return <WebView
+  return <AutoHeightWebView
       style={style}
       source={{ html: getContent(options) }}
       onLoad={onLoad}
       onError={onError}
       renderError={onError}
+      scrollEnabled={false}
+      scalesPageToFit={true}
+      viewportContent={'width=device-width, user-scalable=no'}
   />;
 }
 
